1430f05f664aed3143113fa4563d1967f878b85a
roritopra <you@example.com1193102598@u.icesi.edu.co>
2023-02-23 21:09:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import express, { request, response } from'express';
+import { Server } from 'socket.io';
+const PORT = 5050;
+
+const app = express();
+const httpServer = app.listen(PORT, () => {
+    console.table(
+        {
+            'Controller:' : 'http://localhost:5050/controller',
+            'Display:' : 'http://localhost:5050/display',
+        }
+    )
+});
+const ioServer = new Server(httpServer, { path: '/real-time' });
+
+//const staticController = express.static('public-controller');
+//const staticDisplay = express.static('public-display');
+
+app.use('/controller', express.static('public-controller'));
+app.use('/display', express.static('public-display'));
+app.use(express.json());
+
+/*___________________________________________
+
+1) Create an endpoint to GET a validation message to test if the endpoint is working
+_____________________________________________ */
+
+app.post('/add-lead', (request, response) => {
+    request.body //{name: 'juan}
+    const bonnus = generateBonnus();
+    response.send(bonnus)
+})
+
+
+/*___________________________________________
+
+2) Create the socket methods to listen the events and emit a response
+It should listen for directions and emit the incoming data.
+_____________________________________________ */
+
+ioServer.on('connection', (socket) => {
+
+
+});
+
+/*___________________________________________
+
+3) Create an endpoint to POST user score and print it
+_____________________________________________ */
